de6b11e9ceca8edc5e1f6451fd821d705b85f04f
jasonbillings <jtb223@scarletmail.rutgers.edu>
2026-05-01 15:18:52 -0400

    Can now search round trip flights, booking flights service is almost there.

--- src/BookingService.java
+++ src/BookingService.java
@@ -2,6 +2,8 @@
 import dao.FlightInstanceDAO;
 import dao.ReservationDAO;
 import dao.TicketDAO;
+import models.Customer;
+import models.FlightInstance;
 
 public class BookingService {
     private final FlightInstanceDAO flightInstanceDAO;
@@ -12,5 +14,8 @@ public class BookingService {
         this.flightInstanceDAO = new FlightInstanceDAO();
         this.reservationDAO = new ReservationDAO();
         this.ticketDAO = new TicketDAO();
+    }
+    public static void bookFlight(Customer customer, FlightInstance flight){
+        
     }
 }
--- src/ui/CustomerMenu.java
+++ src/ui/CustomerMenu.java
@@ -1,14 +1,12 @@
 package ui;
 
-import dao.FlightDAO;
 import dao.FlightInstanceDAO;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import models.Customer;
-import models.Flight;
 import models.FlightInstance;
-
+import src.BookingService;
 
 public class CustomerMenu {
     public static void show(Customer customer, Scanner scanner){
@@ -17,7 +15,7 @@ public class CustomerMenu {
         while(running){
             System.out.println("Welcome to the customer menu!");
             System.out.println();
-            System.out.println("1. Search for flights between airports A and B.");
+            System.out.println("1. Search for flights");
             System.out.println("2. Book Flight");
             System.out.println("3. View my Reservations");
             System.out.println("4. Cancel Reservation");
@@ -28,7 +26,7 @@ public class CustomerMenu {
 
             switch (choice) {
                 case "1":
-                    searchFlights(scanner);
+                    searchFlights(customer,scanner);
                     break;
                 case "2":
                     //bookFlights(scanner);
@@ -52,7 +50,7 @@ public class CustomerMenu {
         }
     }
 
-    private static void searchFlights(Scanner scanner){
+    private static void searchFlights(Customer customer, Scanner scanner){
         boolean running = true;
         while (running) {
             System.out.println("Welcome to the flight search menu!");
@@ -66,40 +64,18 @@ public class CustomerMenu {
             
             switch (choice) {
                 case "1":
-                    handleSearchOneWayFlights(scanner);
+                    handleSearchOneWayFlights(customer,scanner);
                     break;
                 case "2":
-                    // handleSearchRoundTripFlights(scanner);
+                    handleSearchRoundTripFlights(customer, scanner);
                     break;
                 default:
                     System.out.println("Invalid option...");                
             }
         }
-
-        System.out.println("Please enter the departure airport: ");
-        String depAirport = scanner.nextLine();
-        System.out.println("Please enter the destination airport");
-        String destAirport = scanner.nextLine();
-        
-
-        FlightDAO dao = new FlightDAO();
-        List<Flight> flights = new ArrayList<>();
-        flights = dao.searchFlights(depAirport, destAirport);
-
-        if(flights.isEmpty()){
-            System.out.println("Sorry, no flights available.");
-        }
-        else{
-            for (Flight f : flights) {
-                System.out.println(
-                    f.getAirlineId() + " " + f.getFlightNumber() + " " +
-                    f.getDepartureAirport() + " -> " + f.getArrivalAirport() +
-                    " " + f.getDepartureTime() + " to " + f.getArrivalTime());
-            }
-        }
     }
 
-    private static void handleSearchOneWayFlights(Scanner scanner){
+    private static void handleSearchOneWayFlights(Customer customer, Scanner scanner){
         System.out.println("Please enter the departure airport: ");
         String depAirport = scanner.nextLine();
         System.out.println("Please enter the destination airport");
@@ -121,8 +97,8 @@ public class CustomerMenu {
         if(flights.isEmpty()){
             System.out.println("Sorry, no flights available.");
         }
-        else{
-            System.out.println("====================== \n Here are the available flights: \n (\"====================== \n");
+        else{ // TODO: display cost of the flights, this may require updating the searchFlight method if not done so already.
+            System.out.println("================================ \n Here are the available flights: \n================================ \n");
             for (FlightInstance f : flights) {
                 System.out.println(
                     f.getAirlineId() + " " + f.getFlightNumber() + " " +
@@ -130,9 +106,16 @@ public class CustomerMenu {
                     " " + f.getDepartureDateTime() + " to " + f.getArrivalDateTime());
             }
         }
+
+        System.out.println("\n================================\n");
+        System.out.println("Would you like to book one of these flights? (y/n)");
+        String choice = scanner.nextLine();
+        if(choice.equalsIgnoreCase("y")){
+            bookFlights(customer, scanner, false);
+        }
     }
 
-    private static void handleSearchRoundTripFLights(Scanner scanner){
+    private static void handleSearchRoundTripFlights(Customer customer, Scanner scanner){
         System.out.println("Please enter the departure airport: ");
         String depAirport = scanner.nextLine();
         System.out.println("Please enter the destination airport");
@@ -144,15 +127,57 @@ public class CustomerMenu {
         System.out.println("Is your schedule flexible? (y/n)");
         String flexible = scanner.nextLine();
 
+        FlightInstanceDAO dao = new FlightInstanceDAO();
+        List<FlightInstance> depFlights = new ArrayList<>();
+        List<FlightInstance> retFlights = new ArrayList<>();
 
-
+        if(flexible.equalsIgnoreCase("y")){
+            depFlights = dao.searchFlights(depAirport, destAirport, depDate, true);
+            retFlights = dao.searchFlights(destAirport, depAirport, retDate, true);
+        }
+        else{
+            depFlights = dao.searchFlights(depAirport, destAirport, depDate, false);
+            retFlights = dao.searchFlights(destAirport, depAirport, retDate, false);
+        }
+        // TODO: display the cost of the flight as well, this may require updating the searchFlight method if not done already
+        System.out.println("================================\n Here are the available departures\n================================");
+        for (FlightInstance f : depFlights) {
+                System.out.println(
+                    f.getAirlineId() + " " + f.getFlightNumber() + " " +
+                    f.getDepartureAirport() + " -> " + f.getArrivalAirport() +
+                    " " + f.getDepartureDateTime() + " to " + f.getArrivalDateTime());
+            }
+        System.out.println("================================\n Here are the available return flights\n================================");
+        for (FlightInstance f : retFlights) {
+                System.out.println(
+                    f.getAirlineId() + " " + f.getFlightNumber() + " " +
+                    f.getDepartureAirport() + " -> " + f.getArrivalAirport() +
+                    " " + f.getDepartureDateTime() + " to " + f.getArrivalDateTime());
+        } // could rewrite this loop as a function but I am too lazy, copy and paste is easy enough
+        System.out.println("Would you like to a pair of these flights? (y/n)");
+        String choice = scanner.nextLine();
+        if (choice.equalsIgnoreCase("y")){
+            bookFlights(customer, scanner, true);
+        }
     }
 
-    private static void bookFlights(Customer customer, Scanner scanner){
-        System.out.println("Enter airline id: ");
-        String airlineId = scanner.nextLine();
-        System.out.println("Enter flight number: ");
-        int flightNum = scanner.nextInt();
-        System.out.println("Enter");
+    private static void bookFlights(Customer customer, Scanner scanner, boolean isRoundTrip){
+        if(isRoundTrip){
+            System.out.println("Please enter the airline id for the departure flight: ");
+            String depAirlineId = scanner.nextLine();
+            System.out.println("Please enter the flight number for the departure flight: ");
+            int depFlightNum = scanner.nextInt();
+            System.out.println("Please enter the airline id for the return flight: ");
+            String retAirlineId = scanner.nextLine();
+            System.out.println("Please enter the flight number for the return flight: ");
+            int retFlightNum = scanner.nextInt();
+        }
+        else{
+            System.out.println("Please enter the airline id for the departure flight: ");
+            String depAirlineId = scanner.nextLine();
+            System.out.println("Please enter the flight number for the departure flight: ");
+            int depFlightNum = scanner.nextInt();
+            // bookFlight(customer)
+        }
     }
 }
